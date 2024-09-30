ed09f14b2df33b9d78a9e1f3cf8197e4348196bb
R0b0so <ethansagar222@gmail.com>
2024-09-29 19:35:53 -0700

    yes

---
+++ change.py
@@ -0,0 +1,25 @@
+name = "orange"
+age = 7
+is_alive = True
+weight = 4.5
+
+print("Name :", name)
+print("Data type of name is", type(name))
+
+print("Age :", age)
+print("Data type of age is", type(age))
+
+print("Is_alive :", is_alive)
+print("Data type of is_alive is", type(is_alive))
+
+print("Weight :", weight)
+print("Data type of weight is", type(weight))
+
+print("\n After type casting...")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
+
